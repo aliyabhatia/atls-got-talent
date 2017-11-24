b46b671c1d85c912153b984f7c2666d15f4b430c
Aliya Bhatia <bhatia.aliya@gmail.com>
2017-11-24 10:44:38 -0500

    separating Listing DB object into listing.py

--- scraper.py
+++ scraper.py
@@ -5,6 +5,9 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from bs4 import BeautifulSoup as soup
 
+# import Listing db class object
+from listing import Listing
+
 # configure flask app and SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -12,23 +15,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
-##### HELPER FUNCTIONS AND CLASSES THAT ARE USED THROUGHOUT #####
-
-# Setup SQL database and object
-class Listing(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    job_title = db.Column(db.String, unique=False, nullable=False)
-    job_link = db.Column(db.String, unique=False, nullable=False)
-    org_name = db.Column(db.String, unique=False, nullable=False)
-    source = db.Column(db.String, unique=False, nullable=False)
-    date_posted = db.Column(db.DateTime, unique=False, nullable=True)
-
-    def __init__(self, job_title, job_link, org_name, source, date_posted):
-        self.job_title = job_title
-        self.job_link = job_link
-        self.org_name = org_name
-        self.source = source
-        self.date_posted = date_posted
+##### HELPER FUNCTIONS THAT ARE USED THROUGHOUT #####
 
 # Grab Beautiful Soup from each website
 def make_page_soup(my_url):
